82bac18f6a608bc18f05328f05541f51c0f10c74
Kyle MacNeney <kyle.macneney@gmail.com>
2018-01-20 23:32:25 -0500

    comment

--- Trees/validate_bst.py
+++ Trees/validate_bst.py
@@ -13,12 +13,27 @@ class Solution(object):
         """
         return self.dfs(root, None, None)
     
-    def dfs(self, node, maxleft, maxright):
+    def dfs(self, node, minleft, maxright):
+        """
+        It's good to note the idea behind this solution
+        ----
+        At any level in a BST you can compare the largest right value seen
+        and the smallest left value seen to the current node to identify 
+        if the tree is valid or not
+
+                    5
+            1           7
+                6
+
+        at node 6 the maxright = 5 
+        and minleft = 1
+        6 is greater than 5 so this evaluates to False
+        """
         if not node:
             return True
-        if maxleft is not None and node.val >= maxleft:
+        if maxleft is not None and node.val >= minleft:
             return False
         if maxright is not None and node.val <= maxright:
             return False
         return self.dfs(node.left, node.val, maxright) and \
-    self.dfs(node.right, maxleft, node.val)
+    self.dfs(node.right, minleft, node.val)
