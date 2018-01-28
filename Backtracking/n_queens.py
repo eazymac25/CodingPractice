69ec01e96d81c2ca260927043d757d9a4ce49e5b
Kyle MacNeney <kyle.macneney@gmail.com>
2018-01-27 21:44:33 -0500

    updating n_queens

--- Backtracking/n_queens.py
+++ Backtracking/n_queens.py
@@ -1,6 +1,5 @@
 """
-The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
-
+The n-queens puzzle is the problem of placing n queens on an nxn chessboard such that no two queens attack each other.
 
 
 Given an integer n, return all distinct solutions to the n-queens puzzle.
