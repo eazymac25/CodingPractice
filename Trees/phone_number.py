62a3b36b04c918d6322d4a3e8c06ded7166069e7
Kyle MacNeney <eazymac25@yahoo.com>
2018-02-21 20:56:32 -0500

    updating comments

--- Trees/phone_number.py
+++ Trees/phone_number.py
@@ -12,26 +12,30 @@ Although the above answer is in lexicographical order, your answer could be in a
 """
 
 class Solution(object):
-    digit_map = {
-        1: "",
-        2: "abc",
-        3: "def",
-        4: "ghi",
-        5: "jkl",
-        6: "mno",
-        7: "pqrs",
-        8: "tuv",
-        9: "wxyz",
-        0: ""
-    }
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
 
-        The idea is that as you go through the digits
-        there are multiple pathways that begin to fan out.
-        This indicates a tree structure of digits, so a dfs
+    digit_map = {
+        "1": "",
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+        "0": ""
+    }
+    def letter_combinations(self, digits):
+        """
+        Args:
+            digits (str): a string of numbers
+        Returns:
+            list[str] a list of all letter combinations
+            associated with the input digits
+
+        The idea is to go through the digits
+        and trace each pathway of possible letters.
+        This indicates a tree structure of results, so a dfs
         was used to construct all combinations.
         """
         if len(digits) == 0:
@@ -39,10 +43,18 @@ class Solution(object):
         return self.dfs(digits, curr="", results=[])
 
     def dfs(self, digits, curr="", results=[]):
+        """
+        Args:
+            digits (str): a string of numbers
+            curr (str): string representing the current combination of letters
+            results (list[str]): storing all the combinations
+        Returns:
+            list[str] a list of all cominations
+        """
         if len(digits) == 0:
             results.append(curr)
             return
-        digit = int(digits[0])
+        digit = digits[0]
         possible = self.digit_map.get(digit)
         for letter in possible:
             n_curr = curr + letter
